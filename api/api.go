3cd14e67e9be4d8348da68b044b2b8843fcb9e3d
Matthias <matthias.rupp@posteo.de>
2020-03-20 18:12:05 +0100

    Add insomnia and models

--- api/api.go
+++ api/api.go
@@ -2,15 +2,25 @@ package api
 
 import (
     "github.com/gin-gonic/gin"
+    "github.com/matthinc/gomment/model"
     "net/http"
+    "fmt"
 )
 
 func routeStatus(c *gin.Context) {
     c.String(http.StatusOK, "running")
 }
 
+func routePostComment(c *gin.Context) {
+    var comment model.Comment
+    c.BindJSON(&comment)
+    //fmt.Println(comment.Content)
+    c.String(http.StatusOK, "ok")
+}
+
 func StartApi() {
     router := gin.Default()
     router.GET("/status", routeStatus)
+    router.POST("/comment", routePostComment)
     router.Run(":8000")
 }
