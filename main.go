c7793e1b005490009ef273b9037daa3bcb07a1b7
Basti Destruction <bastidest@mailbox.org>
2021-10-24 10:17:28 +0200

    update dependencies, apply formatting

--- main.go
+++ main.go
@@ -1,39 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"os"
-    "fmt"
-    "syscall"
+	"syscall"
 
-    "golang.org/x/crypto/ssh/terminal"
-	
-    "github.com/matthinc/gomment/api"
-    "github.com/matthinc/gomment/persistence"
-    "github.com/matthinc/gomment/logic"
-    "github.com/matthinc/gomment/auth"
+	"golang.org/x/crypto/ssh/terminal"
+
+	"github.com/matthinc/gomment/api"
+	"github.com/matthinc/gomment/auth"
+	"github.com/matthinc/gomment/logic"
+	"github.com/matthinc/gomment/persistence"
 )
 
 func main() {
-    if len(os.Args) > 1 {
-        command := os.Args[1]
-        switch command {
-        case "genpw":
-            genpw()
-        }
-    }
-    
-	db := persistence.DB {}
+	if len(os.Args) > 1 {
+		command := os.Args[1]
+		switch command {
+		case "genpw":
+			genpw()
+		}
+	}
 
-    dbPath := os.Getenv("GOMMENT_DB_PATH")
-    if len(dbPath) == 0 {
-        dbPath = "./gomment.db"
-    }
+	db := persistence.DB{}
 
-    pwHash := os.Getenv("GOMMENT_PW_HASH")
-    if len(pwHash) == 0 {
-        fmt.Println("admin password hash variable was not provided (GOMMENT_PW_HASH), disabling administration")
-    }
-    
+	dbPath := os.Getenv("GOMMENT_DB_PATH")
+	if len(dbPath) == 0 {
+		dbPath = "./gomment.db"
+	}
+
+	pwHash := os.Getenv("GOMMENT_PW_HASH")
+	if len(pwHash) == 0 {
+		fmt.Println("admin password hash variable was not provided (GOMMENT_PW_HASH), disabling administration")
+	}
+
 	err := db.Open(dbPath)
 	if err != nil {
 		os.Exit(2)
@@ -43,23 +43,23 @@ func main() {
 		os.Exit(3)
 	}
 
-    logic := logic.Create(&db, pwHash)
-    
-    api.StartApi(&logic)
+	logic := logic.Create(&db, pwHash)
+
+	api.StartApi(&logic)
 }
 
 func genpw() {
-    fmt.Print("Enter your password: ")
-    bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
-    if err != nil {
-        fmt.Println("failed to read password")
-        os.Exit(1)
-    }
-    hash, err := auth.HashPw(string(bytePassword))
-    if err != nil {
-        fmt.Println("failed to hash password")
-        os.Exit(1)
-    }
-    fmt.Println("\n" + hash)
-    os.Exit(0)
+	fmt.Print("Enter your password: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		fmt.Println("failed to read password")
+		os.Exit(1)
+	}
+	hash, err := auth.HashPw(string(bytePassword))
+	if err != nil {
+		fmt.Println("failed to hash password")
+		os.Exit(1)
+	}
+	fmt.Println("\n" + hash)
+	os.Exit(0)
 }
