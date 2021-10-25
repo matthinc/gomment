ac0df3dc7c89fc7a64897d164497cea9eb8b243f
Basti Destruction <bastidest@mailbox.org>
2021-10-25 21:40:46 +0200

    admin: add basic hydration and DOM-based data model

--- frontend/admin/gomment-admin.js
+++ frontend/admin/gomment-admin.js
@@ -9,6 +9,106 @@ function updateTimeElementToLocalTime(timeElement) {
   }
 }
 
+class EventBus {
+  constructor() {
+    this.subscribers = [];
+  }
+
+  on(eventName, callback) {
+    this.subscribers.push({eventName, callback});
+  }
+
+  emit(eventName, eventData) {
+    this.subscribers
+      .filter(s => s.eventName === eventName)
+      .forEach(s => s.callback(eventData));
+  }
+}
+
+class ApiClient {
+  /**
+   * Delete the comment with the given id
+   * @param {string} commentId - comment id to delete
+   * @returns {Promise<void>}
+   */
+  async deleteComment(commentId) {
+    return new Promise((resolve, reject) => {
+      console.log(`stub: delete comment ${commentId}`);
+      resolve();
+    });
+  }
+}
+
+class CommentRow {
+  /**
+   * @constructor
+   * @param {any} tableRow - a HTML table row containing one comment
+   * @param {EventBus} bus - simple event bus
+   */
+  constructor(tableRow, bus) {
+    if(!(tableRow instanceof HTMLTableRowElement)) {
+      throw new Error('the provided element is not a valid HTMLTableRowElement');
+    }
+    /** @type HTMLTableRowElement */
+    this.tableRow = tableRow;
+
+    /** @type EventBus */
+    this.bus = bus;
+
+    const id = tableRow.getAttribute('data-id');
+    if(typeof id !== 'string') {
+      throw new Error('CommentRow does not contain valid id');
+    }
+    /** @type string */
+    this.id = id;
+
+    /** @type boolean */
+    this.isEdit = false;
+
+    this.hydrate();
+  }
+
+  hydrate() {
+    this.tableRow.querySelector('a[href="#delete"]').addEventListener('click', e => {
+      e.preventDefault();
+      this.bus.emit('delete', this.id);
+    });
+
+    this.tableRow.querySelector('a[href="#edit"]').addEventListener('click', e => {
+      e.preventDefault();
+      this.enableEditMode();
+    });
+  }
+
+  /**
+   * Get the id of the comment belonging to this row.
+   * @returns {string}
+   */
+  getId() {
+    return this.id;
+  }
+
+  /**
+   * Enable the edit mode
+   */
+  enableEditMode() {
+    if(this.isEdit) {
+      return;
+    }
+
+    /** @type HTMLParagraphElement */
+    const commentParagraph = this.tableRow.querySelector('.gmt__comment p');
+    const parent = commentParagraph.parentElement;
+    const commentText = commentParagraph.textContent;
+    const commentTextarea = document.createElement('textarea');
+    commentTextarea.innerText = commentText;
+    parent.removeChild(commentParagraph);
+    parent.appendChild(commentTextarea);
+
+    this.isEdit = true;
+  }
+}
+
 export class GommentAdmin {
   /**
    * @constructor
@@ -16,6 +116,12 @@ export class GommentAdmin {
   constructor() {
     /** @type {HTMLTableElement | null} */
     this.table = null;
+
+    /** @type {ApiClient} */
+    this.api = new ApiClient();
+
+    /** @type {EventBus} */
+    this.bus = new EventBus();
   }
 
   /**
@@ -34,6 +140,10 @@ export class GommentAdmin {
       this.table = options.table;
     }
 
+    this.bus.on('delete', (commentId) => {
+      this.deleteComment(commentId);
+    });
+
     this.hydrate();
   }
 
@@ -42,5 +152,22 @@ export class GommentAdmin {
    */
   hydrate() {
     this.table.querySelectorAll('time').forEach(timeEl => updateTimeElementToLocalTime(timeEl));
+    this.table.querySelectorAll('tr.gmt__row').forEach(row => {
+      new CommentRow(row, this.bus);
+    });
+  }
+
+  /**
+   * Delete a comment, ask for confirmation before
+   * @param {string} commentId
+   */
+  deleteComment(commentId) {
+    this.api.deleteComment(commentId)
+      .then(() => {
+        console.log('delete ok');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 }
